b319b8af8f6ade0e8943cb9b587bb3b6f0d6dc24
Sabbir <webexpertsabbir@gmail.com>
2022-08-27 08:51:09 +0600

    explore api

---
+++ js/json.js
@@ -0,0 +1,24 @@
+const user = {id: 1, name: 'Sabbir Hossen', job: 'Programming'}
+// JavaScript Object Notation (JSON)
+
+const stiringify = JSON.stringify(user);
+// console.log(user);
+// console.log(stiringify);
+
+const shop = {
+    name: 'Alia',
+    addres:{
+        street: 'Kocu khat bhuter bari',
+        city: 'ranbir',
+        state: 'bd' 
+    },
+    product:['Laptop', 'Monitor', 'Mic'],
+    revance: 5000,
+    isOpen: true,
+    isOff: false
+}
+console.log(shop);
+const shopJSON = JSON.stringify(shop);
+console.log(shopJSON);
+const shopReset = JSON.parse(shopJSON);
+console.log(shopReset); 
